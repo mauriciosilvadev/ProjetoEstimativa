bce83472b8645f1066a24a91783fab3c9cb53382
Mauricio Silva <mauricio.s.dev@gmail.com>
2025-03-07 00:49:17 -0300

    revertendo a lógica de login

--- src/main/java/br/projeto/state/login/LoginNaoAutenticadoState.java
+++ src/main/java/br/projeto/state/login/LoginNaoAutenticadoState.java
@@ -1,25 +1,23 @@
 package br.projeto.state.login;
 
+import com.pss.senha.validacao.ValidadorSenha;
+
 import br.projeto.presenter.LoginPresenter;
 
 public class LoginNaoAutenticadoState implements LoginState {
 
     @Override
     public void handle(LoginPresenter presenter) {
-        // ValidadorSenha validadorSenha = new ValidadorSenha();
-        // if (!validadorSenha.validar(presenter.getView().getTxtSenha().getText()).isEmpty()) {
+        ValidadorSenha validadorSenha = new ValidadorSenha();
+        if (!validadorSenha.validar(presenter.getView().getTxtSenha().getText()).isEmpty()) {
 
-        //     System.out.println("Senha fora do padrão");
-        //     //presenter.getView().getLblMensagem().setText("Senha fora do padrão");
-        //     //return;
-        // }
-        // boolean isAuthenticated = presenter.getService().autenticar(
-        //         presenter.getView().getTxtEmail().getText(),
-        //         presenter.getView().getTxtSenha().getText()
-        // );
+            System.out.println("Senha fora do padrão");
+            //presenter.getView().getLblMensagem().setText("Senha fora do padrão");
+            //return;
+        }
         boolean isAuthenticated = presenter.getService().autenticar(
-                "mauricio.s.dev@gmail.com",
-                "secret"
+                presenter.getView().getTxtEmail().getText(),
+                presenter.getView().getTxtSenha().getText()
         );
 
         if (isAuthenticated) {
